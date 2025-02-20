81de41a4dd4cffadd9df90d0d8cc7f2512b4606c
Keya Loding <loding.keya@gmail.com>
2025-02-20 17:59:04 -0600

    implemented batch files, fixed most input issues

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# shell
+# Unix-like Shell
+
+## How to Use
+
+Run the following commands in the terminal:
+
+```Unix
+git clone https://github.com/keyaloding/shell.git
+shell/play
+```
--- myshell.c
+++ myshell.c
@@ -8,36 +8,39 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
-#define MAX_ARGS 200
-
-/* Prints error message but does not kill the process. */
-void raise_error() {
-  char* error_message = "An error has occurred\n";
-  write(STDOUT_FILENO, error_message, strlen(error_message));
-}
+#define MAX_ARGS 400
 
 void myPrint(char *msg) {
   write(STDOUT_FILENO, msg, strlen(msg));
 }
 
+/* Prints error message but does not kill the process. */
+void raise_error() {
+  char* error_message = "An error has occurred\n";
+  myPrint(error_message);
+}
+
 void basic_redirect(char* msg, char* filename) {
-  int fd;
-  fd = open(filename, O_RDONLY);
-  if (fd > 0) {
-    printf("Error 1\n");
-    raise_error();
+  pid_t forkret = fork();
+  if (forkret < 0) raise_error();
+  else if (forkret == 0) {
+    int fd = open(filename, O_RDONLY);
+    if (fd > 0) {
+      raise_error();
+      return;
+    }
+    fd = open(filename, O_CREAT | O_WRONLY);
+    if (fd < 0) {
+      raise_error();
+    }
+    ssize_t num_bytes = write(fd, msg, strlen(msg));
+    if (num_bytes < 0) {
+      raise_error();
+    }
+    close(fd);
+  } else {
+    wait(NULL);
   }
-  fd = open(filename, O_CREAT | O_WRONLY);
-  if (fd < 0) {
-    printf("Error 2\n");
-    raise_error();
-  }
-  ssize_t num_bytes = write(fd, msg, strlen(msg));
-  if (num_bytes < 0) {
-    printf("Error 3\n");
-    raise_error();
-  }
-  close(fd);
 }
 
 void adv_redirect(char* msg, char* filename) {
@@ -50,19 +53,25 @@ void adv_redirect(char* msg, char* filename) {
 
 }
 
-void run_pwd(char* args[]) {
+enum redirect {
+  NONE,
+  BASIC,
+  ADV
+};
+typedef enum redirect redirect;
+
+void run_pwd(char* args[], redirect type) {
   char dir[512];
   getcwd(dir, 512);
-  for (int i = 0; args[i]; i++) {
-    if (!strcmp(args[i], ">")) {
-      printf("Basic redirect\n");
-      basic_redirect(dir, args[i+1]);
-      return;
-    } else if (!strcmp(args[i], ">+")) {
-      adv_redirect(dir, args[i+1]);
-      return;
-    }
-  }
+  switch (type) {
+    case NONE:
+      break;
+    case BASIC:
+
+      break;
+    case ADV:
+      break;
+  };
   myPrint(dir);
   myPrint("\n");
 }
@@ -80,90 +89,76 @@ void run_cd(char* args[]) {
   }
 }
 
-/* Runs all commands other than exit, cd, and pwd. */
 void run_cmd(char* args[]) {
-  pid_t pid = fork();
-  if (pid < 0) {
-    raise_error();
-  } else if (pid == 0) {
-    if (execvp(args[0], args) < 0) {
-      raise_error();
-    }
+  if (!strcmp(args[0], "pwd")) {
+    run_pwd(args, NONE);
+  } else if (!strcmp(args[0], "cd")) {
+    run_cd(args);
+  } else if (!strcmp(args[0], "exit")) {
+    exit(0);
   } else {
-    wait(NULL);
+    pid_t pid = fork();
+    if (pid < 0) {
+      raise_error();
+    } else if (pid == 0) {
+      if (execvp(args[0], args) < 0) {
+        raise_error();
+      }
+    } else {
+      wait(NULL);
+    }
   }
 }
 
 /* Converts the input string to an array of arguments. */
-void parse_input(char* input, char* args[]) {
+void parse_input(char* input, char* args[], char* delimiter) {
   char *token;
   int i = 0;
-  while ((token = strsep(&input, " \n\t")) != NULL) {
+  while ((token = strsep(&input, delimiter)) != NULL) {
     if (*token == '\0') continue;
     args[i++] = token;
     if (i >= MAX_ARGS - 1) break;
   }
   args[i] = NULL;
-  // i = 0;
-  // while (args[i]) {
-  //   printf("arg[%d]: %s\n", i, args[i]);
-  //   i++;
-  // }
 }
 
-// void parse_input2(char* input, char* args[]) {
-//   char* token;
-//   int i = 0;
-//   while ((token = strsep(&input, " ")) != NULL) {
-//     if (*token == '\0') {
-//       continue;
-//     }
-//     if (strcmp(token, ";") == 0 || strcmp(token, ">") == 0) {
-//       args[i++] = token;
-//     } else {
-//       char *subtoken;
-//       while ((subtoken = strsep(&token, " ;>")) != NULL) {
-//         if (*subtoken == '\0') {
-//           continue;
-//         }
-//         args[i++] = subtoken;
-//       }
-//     }
-//     if (i >= MAX_ARGS - 1) {
-//       break;
-//     }
-//   }
-//   args[i] = NULL;
-//   i = 0;
-//   while (args[i]) {
-//     printf("arg[%d]: %s\n", i, args[i]);
-//     i++;
-//   }
-// }
 
-void run_multiple_cmds(char* argv) {
-
+void run_multiple_cmds(char* input) {
+  char* cmds[MAX_ARGS];
+  parse_input(input, cmds, ";\n");
+  for (int i = 0; cmds[i]; i++) {
+    char* args[MAX_ARGS];
+    parse_input(cmds[i], args, " \n\t");
+    run_cmd(args);
+  }
+}
+  
+void run_batch_file(char* args[]) {
+  int fd = open(args[1], O_RDONLY);
+  char buffer[1024];
+  if (fd < 0) {
+    raise_error();
+    exit(1);
+  }
+  read(fd, buffer, 1024);
+  run_multiple_cmds(buffer);
+  int retval = close(fd);
+  if (retval < 0) raise_error();
 }
 
 int main(int argc, char *argv[]) {
+  if (argv[1]) {
+    run_batch_file(argv);
+    return 0;
+  }
   char cmd_buff[514];
-  char *pinput, *args[MAX_ARGS];
+  char *pinput;
   while (true) {
     myPrint("myshell> ");
     pinput = fgets(cmd_buff, 514, stdin);
-    if (!pinput) {
-      exit(0);
-    }
-    parse_input(cmd_buff, args);
-    if (!strcmp(args[0], "pwd")) {
-      run_pwd(args);
-    } else if (!strcmp(args[0], "cd")) {
-      run_cd(args);
-    } else if (!strcmp(args[0], "exit")) {
-      exit(0);
-    } else {
-      run_cmd(args);
-    }
+    if (!pinput) exit(0);
+    else if (!strcmp(pinput, "\n")) continue;
+    run_multiple_cmds(pinput);
   }
   return 0;
 }
